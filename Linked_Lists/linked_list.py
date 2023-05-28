335f496167d60e40597e6d2aa4198e93b699a50a
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-05-28 19:58:54 +0200

    linked list

---
+++ Linked_Lists/linked_list.py
@@ -0,0 +1,21 @@
+print("Linked list")
+
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+
+if __name__ == "__main__":
+    linked_list = LinkedList(4)
+    print(linked_list.head.value)
