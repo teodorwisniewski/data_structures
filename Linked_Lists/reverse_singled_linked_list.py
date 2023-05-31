f4d3026008e246e6b2cfac76d17431f9f1d3ce69
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-05-31 10:13:24 +0200

    funcitonal solution

--- Linked_Lists/reverse_singled_linked_list.py
+++ Linked_Lists/reverse_singled_linked_list.py
@@ -3,13 +3,23 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    # O(1)
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        curr, prev = head, None
-        while curr:
-            nxt = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nxt
-        return prev
+# class Solution:
+#     # O(1)
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         curr, prev = head, None
+#         while curr:
+#             nxt = curr.next
+#             curr.next = prev
+#             prev = curr
+#             curr = nxt
+#         return prev
+
+def reverse_list(head):
+    new_head = None
+    current = head
+    while current is not None:
+        nxt_node = current.next
+        current.next = new_head
+        new_head = current
+        current = nxt_node
+    return new_head
