497489cf54772509293744b9c9f5ecfe7c213b5e
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-06-05 21:19:42 +0200

    append doubly linked list

--- Doubly_linked_lists/doubly_linked__list.py
+++ Doubly_linked_lists/doubly_linked__list.py
@@ -13,10 +13,23 @@ class DoublyLinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            self.length = 1
+            return
+        self.tail.next = new_node
+        new_node.prev = self.tail
+        self.tail = new_node
+        self.length += 1
+        return True
         
     def print_list(self):
         current_node = self.head
-        print(f'The head of the linked list is: ', self.length)
+        print(f'The head of the linked list is: ', current_node.value)
         while current_node.next is not None:
             current_node = current_node.next
             print(current_node.value)
@@ -24,4 +37,6 @@ class DoublyLinkedList:
 
 if __name__ == '__main__':
     my_doubly_linked_list = DoublyLinkedList(7)
+    my_doubly_linked_list.append(2)
+    my_doubly_linked_list.append(1)
     my_doubly_linked_list.print_list()
