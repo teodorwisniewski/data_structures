8b62e786e58d260b703d5aceef9dcf772bb05f6b
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-06-14 20:56:43 +0200

    poiping a node from doubly linked list

--- Doubly_linked_lists/doubly_linked_list.js
+++ Doubly_linked_lists/doubly_linked_list.js
@@ -35,90 +35,115 @@ class DoublyLinkedList {
         return this
     }
 
+    pop() {
+       let nodeToPop = this.head;
+       if (this.length === 1){
+            this.head = null;
+            this.tail = null;
+        } else if(this.length > 1){
+            let prev = null;
+            while (nodeToPop.next !== null){
+                prev = nodeToPop;
+                nodeToPop = nodeToPop.next
+            }
+            prev.next = null;
+            nodeToPop.prev = null
+            this.tail = prev;
+        }
+        if (this.length>0){
+            this.length--;
+        }
+        
+        return nodeToPop
+    }
+
     printList() {
-        console.log("\n Printing linked list");
-        let currentNode = this.head;
-        if (this.head === null) {
-            console.log("The DDL is empty");
+        console.log("Printing all nodes in the doubly linked list");
+        if (!this.head){
+            console.log("The doubly linked list is empty.");
             return;
         }
-        console.log('The head of the linked list is: ', currentNode.value);
-        while (currentNode.next !== null){
+        let currentNode = this.head;
+        console.log(`The linked list has ${this.length} nodes`);
+        console.log(`The head of the linked list has the following value ${this.head.value}`);
+        while(currentNode.next !== null){
             currentNode = currentNode.next;
-            console.log(currentNode.value)
+            console.log(`The node value is ${currentNode.value}`);
         }
 
     }
 }
 
 
+console.log("\n\n\n Constructing a new sigly doubly linked list \n")
+let doublyLinkedList = new DoublyLinkedList(4);
+doublyLinkedList.push(6).push(11).push(230);
+doublyLinkedList.printList();
+
+console.log("\n popping nodes from the list \n")
+console.log(`Popping the fillowing node: ${doublyLinkedList.pop().value} `);
+console.log(`Popping the fillowing node: ${doublyLinkedList.pop().value} `);
+console.log(`Popping the fillowing node: ${doublyLinkedList.pop().value} `);
+console.log(`Popping the fillowing node: ${doublyLinkedList.pop().value} `);
+console.log(`Popping the fillowing node: ${doublyLinkedList.pop()} `);
+doublyLinkedList.printList();
+
+// console.log("\n unshift = prepend nodes to the list \n");
+// doublyLinkedList.unshift(9999).unshift(888).unshift(55).unshift(1);
+// doublyLinkedList.printList();
+
+// console.log("\n shift = pop first nodes to the list \n");
+// doublyLinkedList.shift();
+// doublyLinkedList.printList();
+// doublyLinkedList.shift();
+// doublyLinkedList.shift();
+// doublyLinkedList.printList();
+// doublyLinkedList.shift();
+// doublyLinkedList.shift();
+// doublyLinkedList.printList();
+
+// console.log("\n get = get nodes from the list \n");
+// doublyLinkedList.push(1).push(6).push(11).push(230);
+// console.log(`GEtting the fillowing node: ${doublyLinkedList.get(0).value} `);
+// console.log(`GEtting the fillowing node: ${doublyLinkedList.get(1).value} `);
+// console.log(`GEtting the fillowing node: ${doublyLinkedList.get(3).value} `);
+// console.log(`GEtting the fillowing node: ${doublyLinkedList.get(4)} `);
+// console.log(`GEtting the fillowing node: ${doublyLinkedList.get(-3)} `);
+// doublyLinkedList.printList();
 
 
+// console.log("\n set = set nodes from the list \n");
+// doublyLinkedList.set(0, 0);
+// doublyLinkedList.set(1, 100);
+// doublyLinkedList.set(2, 2000);
+// doublyLinkedList.set(3, 30000);
+// doublyLinkedList.set(4, 4423);
+// doublyLinkedList.set(-3, 435);
+// doublyLinkedList.printList();
 
 
+// console.log("\n insert nodes from the list \n");
+// doublyLinkedList.insert(0, 0.00001);
+// doublyLinkedList.insert(1, 101);
+// doublyLinkedList.insert(2, 2002);
+// doublyLinkedList.insert(4, 404);
+// doublyLinkedList.insert(8, 808);
+// doublyLinkedList.insert(10, 2002);
+// doublyLinkedList.printList();
 
 
+// console.log("\n remove nodes from the list \n");
+// console.log(`remove the fillowing node: ${doublyLinkedList.remove(0).value} `);
+// console.log(`remove the fillowing node: ${doublyLinkedList.remove(8)} `);
+// console.log(`remove the fillowing node: ${doublyLinkedList.remove(7).value} `);
+// console.log(`remove the fillowing node: ${doublyLinkedList.remove(2).value} `);
+// console.log(`remove the fillowing node: ${doublyLinkedList.remove(4).value} `);
+// console.log(`remove the fillowing node: ${doublyLinkedList.remove(3).value} `);
+// doublyLinkedList.set(2, 303);
+// doublyLinkedList.set(3, 404);
+// doublyLinkedList.printList();
 
 
-let myDoublyLinkedList = new DoublyLinkedList(7);
-console.log("pushing new nodes to the linked list");
-myDoublyLinkedList.push(2);
-myDoublyLinkedList.push(1);
-myDoublyLinkedList.push(4);
-myDoublyLinkedList.printList();
-console.log("popping last node");
-console.log(myDoublyLinkedList.pop());
-console.log(myDoublyLinkedList.pop());
-console.log(myDoublyLinkedList.pop());
-console.log(myDoublyLinkedList.pop());
-console.log(myDoublyLinkedList.pop());
-console.log(`my_doubly_linked_list length= ${myDoublyLinkedList.length}`);
-myDoublyLinkedList.printList();
-console.log("prepend");
-console.log(myDoublyLinkedList.prepend(5));
-console.log(myDoublyLinkedList.prepend(4));
-console.log(myDoublyLinkedList.prepend(3));
-console.log(myDoublyLinkedList.prepend(2));
-console.log(myDoublyLinkedList.prepend(1));
-console.log(`my_doubly_linked_list length= ${myDoublyLinkedList.length}`);
-myDoublyLinkedList.printList();
-
-console.log("\n\n pop first");
-console.log(myDoublyLinkedList.popFirst());
-console.log(myDoublyLinkedList.popFirst());
-myDoublyLinkedList.printList();
-console.log(myDoublyLinkedList.popFirst());
-console.log(myDoublyLinkedList.popFirst());
-console.log(myDoublyLinkedList.popFirst());
-console.log(myDoublyLinkedList.popFirst());
-myDoublyLinkedList.printList();
-console.log("\n\n get at index");
-console.log(`my_doubly_linked_list length= ${myDoublyLinkedList.length}`);
-console.log(myDoublyLinkedList.prepend(3));
-console.log(myDoublyLinkedList.prepend(2));
-console.log(myDoublyLinkedList.prepend(1));
-console.log(`Getting node at index 0: ${myDoublyLinkedList.get(0)}`);
-console.log(`Getting node at index 1: ${myDoublyLinkedList.get(1)}`);
-console.log(`Getting node at index 2: ${myDoublyLinkedList.get(2)}`);
-console.log(`Getting node at index 5: ${myDoublyLinkedList.get(5)}`);
-console.log(`Getting node at index -5: ${myDoublyLinkedList.get(5)}`);
-myDoublyLinkedList.printList();
-console.log("\n\n set value");
-myDoublyLinkedList.setValue(0, 10);
-myDoublyLinkedList.setValue(1, 20);
-myDoublyLinkedList.setValue(2, 30);
-myDoublyLinkedList.printList();
-console.log("\n\n insert");
-myDoublyLinkedList.insert(0, 0);
-myDoublyLinkedList.insert(4, 4);
-myDoublyLinkedList.insert(2, 222);
-myDoublyLinkedList.printList();
-console.log("\n\n remove");
-console.log("Just removing the following node", myDoublyLinkedList.remove(0));
-myDoublyLinkedList.printList();
-console.log("\n remove node at index 2");
-console.log("Just removing the following node", myDoublyLinkedList.remove(2));
-myDoublyLinkedList.printList();
-console.log("\n remove node at index 3");
-console.log("Just removing the following node", myDoublyLinkedList.remove(3));
-myDoublyLinkedList.printList();
+// console.log("\n reversing the list \n");
+// doublyLinkedList.reverse();
+// doublyLinkedList.printList();
