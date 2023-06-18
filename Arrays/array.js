fd24eec8f1f29d3491406f6b26883a827de9bbbc
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-06-18 22:58:20 +0200

    delete

--- Arrays/array.js
+++ Arrays/array.js
@@ -22,12 +22,30 @@ class MyArray {
         this.length--;
         return lastItem
     }
+
+    delete(index){
+        const item = this.data[index];
+        this.shiftItems(index);
+        this.length--;
+    } 
+
+    shiftItems(index){
+        for(let i = index; i < this.length - 1; i++){
+            this.data[i] = this.data[i+1];
+        }
+        delete this.data[this.length-1]
+    }
 }
 
 
 const newArray = new MyArray();
-newArray.push("Hi")
-newArray.push("there!")
+newArray.push(0)
+newArray.push(1)
+newArray.push(2)
+newArray.push(3)
+newArray.push(4)
+newArray.push(5)
 newArray.push("Last item");
 console.log(newArray.pop())
+newArray.delete(2)
 console.log(newArray)
