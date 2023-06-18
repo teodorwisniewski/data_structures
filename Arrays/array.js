47dc7bfa1cc0759903dfcbbae641dd6b0ead141c
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-06-18 22:50:29 +0200

    pop

--- Arrays/array.js
+++ Arrays/array.js
@@ -18,7 +18,9 @@ class MyArray {
 
     pop(){
         let lastItem = this.data[this.length-1];
-        
+        delete this.data[this.length-1];
+        this.length--;
+        return lastItem
     }
 }
 
@@ -26,4 +28,6 @@ class MyArray {
 const newArray = new MyArray();
 newArray.push("Hi")
 newArray.push("there!")
+newArray.push("Last item");
+console.log(newArray.pop())
 console.log(newArray)
