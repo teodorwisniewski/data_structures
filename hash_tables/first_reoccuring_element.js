82ba3a4f480df4dad580ccae4a49743bfb9ac0ff
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-06-21 08:58:21 +0200

    first reoocrring instance in the array

---
+++ hash_tables/first_reoccuring_element.js
@@ -0,0 +1,24 @@
+
+function firstRecurringCharacter(input) {
+  const recurringChar = {}
+  
+  for (let i = 0; i < input.length; i++) {
+    el = input[i];
+    if (el in recurringChar){
+        return el;
+    }
+    recurringChar[el] = true
+
+  }
+
+  return undefined
+}
+
+let arr1 = [1, 2, 3, 2, 4 , 6, 4];
+console.log(firstRecurringCharacter(arr1));
+
+let arr2 = [2, 1, 1, 2, 4 , 6, 4];
+console.log(firstRecurringCharacter(arr2));
+
+let arr3 = [1, 2, 3, 4, 5];
+console.log(firstRecurringCharacter(arr3));
