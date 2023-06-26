f3911cd2378a79e5f0912ee0347984bb38ba6fe2
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-06-26 07:44:03 +0200

    bst constructor

---
+++ Trees/binary_search_tree.js
@@ -0,0 +1,19 @@
+
+
+class BinaryNode{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+class BST{
+    constructor(){
+        this.root = null;
+        this.left = null;
+        this.right = null;
+    }
+}
+
