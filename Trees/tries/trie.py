ab711af9b53de195012fca193a97b73c6b497c79
Teodor Wisniewski <48416342+teodorwisniewski@users.noreply.github.com>
2023-08-05 14:22:28 +0200

    trie search method

--- Trees/tries/trie.py
+++ Trees/tries/trie.py
@@ -10,3 +10,28 @@ class TrieNode:
 class Trie:
     def __init__(self):
         self.root = TrieNode()
+
+    def search(self, word: str) -> TrieNode:
+        curr_node = self.root
+        for ch in word:
+            if (child_node := curr_node.children.get(ch)):
+                curr_node = child_node
+            else:
+                return None
+        return curr_node
+
+    def insert(self, word):
+        pass
+
+    def collect_words(self, words=None, node=None, word=""):
+        pass
+
+
+words_to_insert = [
+    "cat",
+    "cab",
+    "car",
+    "ace",
+    "act",
+    "add"
+]
